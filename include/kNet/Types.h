69f1b11b2f51b29c687b8331e3df00f4d55acc47
clb-hp <devnull@localhost>
2010-10-15 23:36:59 +0300

    Added C99 and boost-aware fixed-width type implementations.

--- include/kNet/Types.h
+++ include/kNet/Types.h
@@ -16,12 +16,43 @@
 /** @file Types.h
 	@brief Provides platform-independent fixed size types. */
 
-namespace kNet
-{
-///\todo C99.
-typedef unsigned char byte;
-typedef unsigned short word;
-typedef unsigned long dword;
+#ifndef KNET_NO_FIXEDWIDTH_TYPES
+
+// As a reminder: http://predef.sourceforge.net/prestd.html
+
+// If we have C99, take the types from there.
+#if (__STDC_VERSION__ >= 199901L) || (_MSC_VER >= 1600)
+
+#include <cstdint>
+
+typedef uint8_t u8; ///< a single byte: 0-255.
+typedef uint16_t u16; ///< 2 bytes: 0 - 65535.
+typedef uint32_t u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef uint64_t u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
+
+typedef int8_t s8; ///< a single byte: -128 - 127.
+typedef int16_t s16; ///< 2 bytes: -32768 - 32767.
+typedef int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+typedef int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
+
+// Otherwise, if we have boost, we can also pull the types from there.
+#elif KNET_USE_BOOST
+
+#include <boost/cstdint.hpp>
+
+typedef boost::uint8_t u8; ///< a single byte: 0-255.
+typedef boost::uint16_t u16; ///< 2 bytes: 0 - 65535.
+typedef boost::uint32_t u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef boost::uint64_t u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
+
+typedef boost::int8_t s8; ///< a single byte: -128 - 127.
+typedef boost::int16_t s16; ///< 2 bytes: -32768 - 32767.
+typedef boost::int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+typedef boost::int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
+
+#else // No boost or unknown if we have C99. Have to guess the following are correct.
+
+#warning "Not using boost and C99 not defined. Guessing the built-ins for fixed-width types!"
 
 typedef unsigned char u8; ///< a single byte: 0-255.
 typedef unsigned short u16; ///< 2 bytes: 0 - 65535.
@@ -33,7 +64,6 @@ typedef signed short s16; ///< 2 bytes: -32768 - 32767.
 typedef signed int s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
 typedef signed long long s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
 
-typedef unsigned long ulong;
-typedef unsigned int uint;
+#endif
 
-} // ~kNet
+#endif // ~KNET_NO_FIXEDWIDTH_TYPES
