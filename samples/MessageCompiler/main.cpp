e03ed71ec8b2ad2875d31d48a249cce5854e0a51
jj <devnull@localhost>
2010-11-26 13:03:05 +0200

    Added error prints to MessageCompiler sample.

--- samples/MessageCompiler/main.cpp
+++ samples/MessageCompiler/main.cpp
@@ -27,39 +27,51 @@ using namespace kNet;
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-	{
-		cout << "No parameters given." << endl;
-		return 0;
-	}
+    try
+    {
+	    if (argc < 2)
+	    {
+		    cout << "No parameters given." << endl;
+		    return 0;
+	    }
 
-	EnableMemoryLeakLoggingAtExit();
+	    EnableMemoryLeakLoggingAtExit();
 
-	SerializedMessageList msg;
-	msg.LoadMessagesFromFile(argv[1]);
+	    SerializedMessageList msg;
+	    msg.LoadMessagesFromFile(argv[1]);
 
-	cout << "File " << argv[1] << " contains the following structs:" << endl;
+	    cout << "File " << argv[1] << " contains the following structs:" << endl;
 
-	const std::list<SerializedElementDesc> &elements = msg.GetElements();
-	for(std::list<SerializedElementDesc>::const_iterator iter = elements.begin();
-		iter != elements.end(); ++iter)
-	{
-		const SerializedElementDesc &elem = *iter;
-		
-		if (elem.type == SerialStruct)
-			if (elem.name.length() == 0)
-				cout << "<unnamed struct>" << endl;
-			else
-				cout << elem.name << endl;
-	}
+	    const std::list<SerializedElementDesc> &elements = msg.GetElements();
+	    for(std::list<SerializedElementDesc>::const_iterator iter = elements.begin();
+		    iter != elements.end(); ++iter)
+	    {
+		    const SerializedElementDesc &elem = *iter;
+    		
+		    if (elem.type == SerialStruct)
+			    if (elem.name.length() == 0)
+				    cout << "<unnamed struct>" << endl;
+			    else
+				    cout << elem.name << endl;
+	    }
 
-	const std::list<SerializedMessageDesc> &messages = msg.GetMessages();
-	for(std::list<SerializedMessageDesc>::const_iterator iter = messages.begin();
-		iter != messages.end(); ++iter)
-	{
-		const SerializedMessageDesc &msg = *iter;
-		SerializationStructCompiler compiler;	
-		string messageName = compiler.ParseToValidCSymbolName(msg.name.c_str()) + ".h";
-		compiler.CompileMessage(msg, messageName.c_str());
-	}
+	    const std::list<SerializedMessageDesc> &messages = msg.GetMessages();
+	    for(std::list<SerializedMessageDesc>::const_iterator iter = messages.begin();
+		    iter != messages.end(); ++iter)
+	    {
+		    const SerializedMessageDesc &msg = *iter;
+		    SerializationStructCompiler compiler;	
+		    string messageName = compiler.ParseToValidCSymbolName(msg.name.c_str()) + ".h";
+		    compiler.CompileMessage(msg, messageName.c_str());
+	    }
+    } catch(const NetException &e)
+    {
+        cout << "MessageCompiler received NetException: " << e.what() << endl;
+    } catch(const std::exception &e)
+    {
+        cout << "MessageCompiler received std::exception: " << e.what() << endl;
+    } catch(...)
+    {
+        cout << "MessageCompiler received an unknown exception!" << endl;
+    }
 }
