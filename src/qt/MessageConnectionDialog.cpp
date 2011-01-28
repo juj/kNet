e121487be44a857130fd87533684128c7e05e0d8
jj <devnull@localhost>
2011-01-28 16:07:08 +0200

    Added missing files.

---
+++ src/qt/MessageConnectionDialog.cpp
@@ -0,0 +1,63 @@
+/* Copyright 2010 Jukka Jylänki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+/** @file MessageConnectionDialog.cpp
+	@brief */
+
+#include <QUiLoader>
+#include <QFile>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QTreeWidget>
+
+#include "kNet/qt/MessageConnectionDialog.h"
+
+namespace kNet
+{
+
+const int dialogUpdateInterval = 200;
+
+MessageConnectionDialog::MessageConnectionDialog(QWidget *parent, Ptr(MessageConnection) connection_)
+:connection(connection_), QWidget(parent)
+{
+	QUiLoader loader;
+	QFile file("MessageConnectionDialog.ui");
+	file.open(QFile::ReadOnly);
+	QWidget *myWidget = loader.load(&file, this);
+	file.close();
+/*
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(myWidget);
+	setLayout(layout);
+*/
+	updateTimer = new QTimer(this);
+	connect(updateTimer, SIGNAL(timeout()), this, SLOT(Update()));
+	Update();
+}
+
+void MessageConnectionDialog::Update()
+{
+	if (!connection)
+		return;
+
+	QLabel *connectionLine = findChild<QLabel*>("connectionLine");
+	if (!connectionLine)
+		return;
+
+	connectionLine->setText(connection->ToString().c_str());
+
+	updateTimer->start(dialogUpdateInterval);
+}
+
+} // ~kNet
---
+++ src/qt/NetworkDialog.cpp
@@ -0,0 +1,122 @@
+/* Copyright 2010 Jukka Jylänki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+/** @file NetworkDialog.cpp
+	@brief */
+
+#include <QUiLoader>
+#include <QFile>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QTreeWidget>
+
+#include "kNet/qt/NetworkDialog.h"
+#include "kNet/qt/MessageConnectionDialog.h"
+
+namespace kNet
+{
+
+const int dialogUpdateInterval = 1000;
+
+NetworkDialog::NetworkDialog(QWidget *parent, Network *network_)
+:network(network_), QWidget(parent)
+{
+	QUiLoader loader;
+	QFile file("NetworkDialog.ui");
+	file.open(QFile::ReadOnly);
+	QWidget *myWidget = loader.load(&file, this);
+	file.close();
+/*
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(myWidget);
+	setLayout(layout);
+*/
+	QTreeWidget *connectionsTree = findChild<QTreeWidget*>("connectionsTree");
+	if (connectionsTree)
+		connect(connectionsTree, SIGNAL(itemDoubleClicked(QTreeWidgetItem *, int)), this, SLOT(ItemDoubleClicked(QTreeWidgetItem *)));
+
+	updateTimer = new QTimer(this);
+	connect(updateTimer, SIGNAL(timeout()), this, SLOT(Update()));
+	Update();
+}
+
+class MessageConnectionTreeItem : public QTreeWidgetItem
+{
+public:
+	MessageConnectionTreeItem(QTreeWidgetItem *parent, Ptr(MessageConnection) connection_)
+	:QTreeWidgetItem(parent), connection(connection_)
+	{
+		if (connection)
+			setText(0, connection->ToString().c_str());
+	}
+
+	Ptr(MessageConnection) connection;
+};
+
+QTreeWidgetItem *NewTreeItemFromString(const char *str)
+{
+	QTreeWidgetItem *item = new QTreeWidgetItem();
+	item->setText(0, str);
+	return item;
+}
+
+void NetworkDialog::Update()
+{
+	if (!network)
+		return;
+
+	QLabel *machineIp = findChild<QLabel*>("machineIP");
+	if (!machineIp)
+		return;
+	QLabel *numRunningThreads = findChild<QLabel*>("numRunningThreads");
+	if (!numRunningThreads)
+		return;
+	QTreeWidget *connectionsTree = findChild<QTreeWidget*>("connectionsTree");
+	if (!connectionsTree)
+		return;
+
+	machineIp->setText(network->MachineIP());
+	numRunningThreads->setText(QString::number(network->NumWorkerThreads()));
+
+	connectionsTree->clear();
+
+	Ptr(NetworkServer) server = network->GetServer();
+	if (server)
+	{
+		QTreeWidgetItem *serverItem = NewTreeItemFromString(server->ToString().c_str());
+		connectionsTree->addTopLevelItem(serverItem);
+
+		NetworkServer::ConnectionMap connections = server->GetConnections();
+		for(NetworkServer::ConnectionMap::iterator iter = connections.begin(); iter != connections.end(); ++iter)
+		{
+			QTreeWidgetItem *connectionItem = new MessageConnectionTreeItem(serverItem, iter->second);
+			serverItem->addChild(connectionItem);
+			serverItem->setExpanded(true);
+		}
+	}
+
+	updateTimer->start(dialogUpdateInterval);
+}
+
+void NetworkDialog::ItemDoubleClicked(QTreeWidgetItem *item)
+{
+	MessageConnectionTreeItem *msgItem = dynamic_cast<MessageConnectionTreeItem*>(item);
+	if (!msgItem)
+		return;
+
+	MessageConnectionDialog *dialog = new MessageConnectionDialog(0, msgItem->connection);
+	dialog->show();
+}
+
+} // ~kNet
