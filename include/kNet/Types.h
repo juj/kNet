e0aae9ac72fbcaf080abdb2ba55848f60cb5686f
clb <devnull@localhost>
2011-04-30 13:19:52 +0300

    Converted guessed fixed-size types to match with boost definitions.

--- include/kNet/Types.h
+++ include/kNet/Types.h
@@ -36,7 +36,7 @@ typedef int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e
 typedef int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
 
 // Otherwise, if we have boost, we can also pull the types from there.
-#elif KNET_USE_BOOST
+#elif defined(KNET_USE_BOOST)
 
 #include <boost/cstdint.hpp>
 
@@ -52,16 +52,25 @@ typedef boost::int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e1
 
 #else // No boost or unknown if we have C99. Have to guess the following are correct.
 
+#include <limits.h>
+
 //#pragma warning "Not using boost and C99 not defined. Guessing the built-ins for fixed-width types!"
 
 typedef unsigned char u8; ///< a single byte: 0-255.
 typedef unsigned short u16; ///< 2 bytes: 0 - 65535.
-typedef unsigned int u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
 typedef unsigned long long u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
 
 typedef signed char s8; ///< a single byte: -128 - 127.
 typedef signed short s16; ///< 2 bytes: -32768 - 32767.
-typedef signed int s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+
+#if ULONG_MAX == 0xffffffff
+typedef unsigned long u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef long s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+#elif UINT_MAX == 0xffffffff
+typedef unsigned int u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef int s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+#endif
+
 typedef signed long long s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
 
 #endif
