106c93bbd594613e42dcc44aceb44ffe6f34069c
Jukka Jylänki <jujjyl@gmail.com>
2013-03-07 23:28:21 +0200

    Added cppcheck script.

---
+++ cppcheck/run_cppcheck.bat
@@ -0,0 +1,8 @@
+:: The warning unusedFunction: 'The function 'funcName' is never used.' is suppressed because as a generic utility library, most of the functions are not used by the library itself.
+
+"C:\Program Files (x86)\Cppcheck\cppcheck" --version
+
+"C:\Program Files (x86)\Cppcheck\cppcheck" --template "{file}({line}): ({severity}) ({id}): {message}" -UMATH_QT_INTEROP -I../src -rp=../src --enable=all --suppress=unusedFunction --error-exitcode=1 --force --suppressions suppressions.txt ../src
+
+pause
+
