994957b3543b49928ef5be29b5616fdcdfd543a9
Jukka Jylänki <jujjyl@gmail.com>
2014-02-10 23:06:01 +0200

    Pass command line in cmake VS batch files to allow setting options and other flags when calling from command line.

--- cmake_vs2010.bat
+++ cmake_vs2010.bat
@@ -1,5 +1,5 @@
 @rem To explicitly configure the location of Qt, uncomment and specify the following lines.
 @rem SET QMAKESPEC=E:\Qt\2010.02.1\qt\mkspecs\win32-msvc2008
 @rem SET QTDIR=E:\Qt\2010.02.1\qt
-cmake -G "Visual Studio 10"
+cmake -G "Visual Studio 10" %*
 pause
